598c1fc7f714bfeefb6f03034eac9e7ee07f5d9d
Volodymyr Vynnyk <33934184+volodymyrvynnyk@users.noreply.github.com>
2020-12-10 19:19:30 +0200

    Fisrt PR merged
    * created Employee class
    
    * jack works 2 times

---
+++ src/com/company/Employee.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Employee {
+
+    private String name;
+
+    private int workHoursPerDay;
+
+    public Employee(String name, int workHoursPerDay) {
+        this.name = name;
+        this.workHoursPerDay = workHoursPerDay;
+    }
+
+    public void work() {
+        for (int i = 0; i < workHoursPerDay; i++) {
+            System.out.println(i + " hours worked");
+        }
+        System.out.println("Finished");
+    }
+}
